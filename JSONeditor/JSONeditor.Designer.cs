818cbc393c638bfd506a3154274f4daae2f9ea39
mahmoudelwekel <mahmoudelwekel@ymail.com>
2022-02-21 21:19:04 +0200

    first commit

---
+++ JSONeditor/JSONeditor.Designer.cs
@@ -0,0 +1,176 @@
+﻿namespace JSONeditor
+{
+    partial class JSONeditor
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Attach = new System.Windows.Forms.Button();
+            this.DatabaseTextBox = new System.Windows.Forms.TextBox();
+            this.ExtractBtn = new System.Windows.Forms.Button();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.tableLayoutPanel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // Attach
+            // 
+            this.Attach.AutoSize = true;
+            this.Attach.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.Attach.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.Attach.Location = new System.Drawing.Point(13, 16);
+            this.Attach.Margin = new System.Windows.Forms.Padding(3, 6, 3, 6);
+            this.Attach.Name = "Attach";
+            this.Attach.Size = new System.Drawing.Size(54, 36);
+            this.Attach.TabIndex = 0;
+            this.Attach.Text = "Open";
+            this.Attach.UseVisualStyleBackColor = true;
+            this.Attach.Click += new System.EventHandler(this.Attach_Click);
+            // 
+            // DatabaseTextBox
+            // 
+            this.DatabaseTextBox.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
+            this.DatabaseTextBox.Location = new System.Drawing.Point(73, 18);
+            this.DatabaseTextBox.Name = "DatabaseTextBox";
+            this.DatabaseTextBox.ReadOnly = true;
+            this.DatabaseTextBox.Size = new System.Drawing.Size(698, 32);
+            this.DatabaseTextBox.TabIndex = 1;
+            // 
+            // ExtractBtn
+            // 
+            this.ExtractBtn.AutoSize = true;
+            this.ExtractBtn.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.tableLayoutPanel1.SetColumnSpan(this.ExtractBtn, 2);
+            this.ExtractBtn.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.ExtractBtn.Location = new System.Drawing.Point(13, 512);
+            this.ExtractBtn.Name = "ExtractBtn";
+            this.ExtractBtn.Size = new System.Drawing.Size(758, 36);
+            this.ExtractBtn.TabIndex = 4;
+            this.ExtractBtn.Text = "Save";
+            this.ExtractBtn.UseVisualStyleBackColor = true;
+            this.ExtractBtn.Click += new System.EventHandler(this.ExtractBtn_Click);
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.AutoSize = true;
+            this.tableLayoutPanel1.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.tableLayoutPanel1.ColumnCount = 2;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle());
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel1.Controls.Add(this.Attach, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.DatabaseTextBox, 1, 0);
+            this.tableLayoutPanel1.Controls.Add(this.ExtractBtn, 0, 3);
+            this.tableLayoutPanel1.Controls.Add(this.dataGridView1, 0, 2);
+            this.tableLayoutPanel1.Controls.Add(this.textBox1, 1, 1);
+            this.tableLayoutPanel1.Controls.Add(this.label1, 0, 1);
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.Padding = new System.Windows.Forms.Padding(10);
+            this.tableLayoutPanel1.RowCount = 4;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle());
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle());
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle());
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(784, 561);
+            this.tableLayoutPanel1.TabIndex = 5;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.tableLayoutPanel1.SetColumnSpan(this.dataGridView1, 2);
+            this.dataGridView1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.dataGridView1.Location = new System.Drawing.Point(13, 99);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidthSizeMode = System.Windows.Forms.DataGridViewRowHeadersWidthSizeMode.AutoSizeToAllHeaders;
+            this.dataGridView1.Size = new System.Drawing.Size(758, 407);
+            this.dataGridView1.TabIndex = 5;
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBox1.Location = new System.Drawing.Point(73, 61);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(698, 32);
+            this.textBox1.TabIndex = 1;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.label1.Location = new System.Drawing.Point(13, 58);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(54, 38);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Search";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // JSONeditor
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 24F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(784, 561);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.Font = new System.Drawing.Font("Cairo", 10F, System.Drawing.FontStyle.Bold);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.Margin = new System.Windows.Forms.Padding(3, 6, 3, 6);
+            this.Name = "JSONeditor";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "JSONeditor";
+            this.TopMost = true;
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.tableLayoutPanel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button Attach;
+        private System.Windows.Forms.TextBox DatabaseTextBox;
+        private System.Windows.Forms.Button ExtractBtn;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ JSONeditor/JSONeditor.cs
@@ -0,0 +1,97 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.IO;
+using System.Windows.Forms;
+
+namespace JSONeditor
+{
+    public partial class JSONeditor : Form
+    {
+ 
+        public JSONeditor()
+        {
+            InitializeComponent();
+            AttachDatabase();
+        }
+
+        void AttachDatabase()
+        {
+            try
+            {
+                openFileDialog1.Filter = "Json files (*.json)|*.json";
+                if (openFileDialog1.ShowDialog() == DialogResult.OK)
+                { 
+
+                    DatabaseTextBox.Text = openFileDialog1.FileName;
+
+                    var table = JsonConvert.DeserializeObject<DataTable>(File.ReadAllText(DatabaseTextBox.Text));
+                    dataGridView1.DataSource = table;
+
+                }
+            }
+            catch (Exception errortext)
+            {
+                MessageBox.Show(errortext.ToString());
+            }
+        }
+
+      
+
+        private void Attach_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                AttachDatabase();
+            }
+            catch (Exception errortext)
+            {
+                MessageBox.Show(errortext.ToString());
+            }
+        }
+
+        private void ExtractBtn_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string output = JsonConvert.SerializeObject(this.dataGridView1.DataSource,Formatting.Indented);
+
+                System.IO.File.WriteAllText(DatabaseTextBox.Text, output);
+
+                MessageBox.Show("Saved successfully!", DateTime.Now.ToString(), MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            catch (Exception errortext)
+            {
+                MessageBox.Show(errortext.ToString());
+            }
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            try
+            {
+                //var bd = (BindingSource)dataGridView1.DataSource;
+                var dt = (DataTable)dataGridView1.DataSource;
+
+                List<string> QueryList = new List<string>();
+
+                foreach (DataColumn item in dt.Columns)
+                {
+                    string Query = string.Format(item.ColumnName+" like '%{0}%'", textBox1.Text.Trim().Replace("'", "''"));
+                    QueryList.Add(Query);
+                }
+
+
+                dt.DefaultView.RowFilter = string.Join(" OR ", QueryList); 
+                dataGridView1.Refresh();
+            }
+            catch (Exception errortext)
+            {
+                MessageBox.Show(errortext.ToString());
+            }
+        }
+    }
+}
